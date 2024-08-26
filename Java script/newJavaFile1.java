4909fd09e428b85ff462901a68ac628c42773ef9
peksi1234 <sebastian.osterberg06@gmail.com>
2024-08-26 12:31:06 +0300

    Add files via upload

---
+++ Java script/newJavaFile1.java
@@ -0,0 +1,39 @@
+
+public class NewJavaFile {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        // First and last name variables
+        String firstname = "Bob";
+        String lastname = "Longson";
+
+        // Combines the Names into fullname variable
+        String fullname = firstname + " " + lastname;
+        System.out.println(fullname);
+
+        // Additional name example
+        String name = "bob";
+        System.out.println("hello " + name);
+
+        // Two int variables "37" and "3"
+        int x = 37;
+        int y = 3;
+
+        // Addition, subtraction, multiplication, and division
+        System.out.println(x + y); // Addition
+        System.out.println(x - y); // Subtraction
+        System.out.println(x * y); // Multiplication
+        System.out.println(x / y); // Division
+
+        int z = 10;
+        System.out.println(x + z + "y"); // Concatenation
+        
+        boolean isCodingFun = true;
+        boolean funOutDoorDay = false;
+        
+        System.out.println(isCodingFun);
+        System.out.println(funOutDoorDay);
+    }
+}
+
---
+++ Java script/printline,system.java
@@ -0,0 +1,26 @@
+
+public class newJavaFile {
+    
+    public static void main (String args[])  {
+        // Print some text on a new row
+        System.out.println("Hello World");
+        //Print some text on the same now
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        // SOUT + tab to autofill
+        System.out.println("3");
+        System.out.println("358");
+        System.out.println("50000");
+        
+        //basic math
+        System.out.println(3 + 3); //This is a command
+        System.out.println(3 + 3);
+        System.out.println(3 - 3);
+        System.out.println(3 / 3);
+        /* This is a comment
+        System.out.println(3 - 3);
+        System.out.println(3 / 3);
+        */
+                
+    }
+}

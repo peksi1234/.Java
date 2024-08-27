52abd00dcb525e2d855aa09457bd6c7a938faa1c
peksi1234 <sebastian.osterberg06@gmail.com>
2024-08-27 12:34:46 +0300

    Add files via upload

---
+++ uppgifter java/grunddläggandeMatematik.java
@@ -0,0 +1,15 @@
+
+public class grunddläggandeMatematik {
+
+    public static void main(String args[]) {
+         int num1 = 50;
+        int num2 = 20;
+
+        System.out.println(num1 + num2);  
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+
+
+    }
+}
